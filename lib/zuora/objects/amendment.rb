54cdde65a5de761162cbc6bbdd930082349582fb
James W Moline <jmoline@gmail.com>
2012-07-21 09:18:46 -0400

    changes so amendments can handle product types properly, spec coverage, beefing up spec coverage, too

--- lib/zuora/objects/amendment.rb
+++ lib/zuora/objects/amendment.rb
@@ -1,5 +1,9 @@
 module Zuora::Objects
   class Amendment < Base
+    attr_accessor :product_rate_plan
+
+    store_accessors :amend_options
+
     belongs_to :subscription
 
     validates_presence_of :subscription_id, :name
@@ -16,13 +20,78 @@ module Zuora::Objects
     validates_date_of :term_start_date, :if => Proc.new { |a| a.type == 'TermsAndConditions' }
     validates_presence_of :destination_account_id, :if => Proc.new {|a| a.type == 'OwnerTransfer' }
     validates_presence_of :destination_invoice_owner_id, :if => Proc.new {|a| a.type == 'OwnerTransfer' }
+    validates_presence_of :product_rate_plan, :if => Proc.new {|a| ["NewProduct","RemoveProduct","UpdateProduct"].include?(a.type) }
     validates_inclusion_of :status, :in => ["Completed", "Cancelled", "Draft", "Pending Acceptance", "Pending Activation"]
     validates_inclusion_of :term_type, :in => ['TERMED', 'EVERGREEN'], :allow_nil => true
     validates_inclusion_of :type, :in => ['Cancellation', 'NewProduct', 'OwnerTransfer', 'RemoveProduct', 'Renewal', 'UpdateProduct', 'TermsAndConditions']
 
+    validate do |request|
+      request.must_have_existing(:product_rate_plan)
+    end
+
     define_attributes do
       read_only :created_by_id, :created_date, :updated_by_id, :updated_date
       defaults :status => 'Draft'
+    end
+
+    def must_have_existing(ref)
+      object = self.send(ref)
+      unless object.blank?
+        errors[ref] << "is invalid" if object.id.nil?
+      end
+    end
+
+    def rate_plan_designation
+      ["RemoveProduct","UpdateProduct"].include?(type) ? :AmendmentSubscriptionRatePlanId : :ProductRatePlanId
+    end
+
+    def create
+      return false unless valid?
+      result = Zuora::Api.instance.request(:amend) do |xml|
+        xml.__send__(zns, :requests) do |s|
+          s.__send__(zns, :Amendments) do |a|
+            to_hash.each do |k,v|
+              a.__send__(ons, k.to_s.camelize.to_sym, (v.is_a?(Date)||v.is_a?(Time) ? v.to_datetime.strftime("%FT%T%:z") : v)) unless v.nil?
+            end
+            generate_rate_plan(a)
+          end
+          s.__send__(zns, :AmendOptions) do |ao|
+            generate_amend_options(ao)
+          end unless amend_options.blank?
+        end
+      end
+      apply_response(result.to_hash)
+    end
+
+    def apply_response(response_hash)
+      # really lame but the amend method returns "results"
+      # instead of "result" like every other call
+      result = response_hash[:amend_response][:results]
+      if result[:success]
+        self.id = result[:id]
+        @previously_changed = changes
+        @changed_attributes.clear
+        return true
+      else
+        self.errors.add(:base, result[:errors][:message])
+        return false
+      end
+    end
+
+    def generate_rate_plan(builder)
+      if product_rate_plan.present?
+        builder.__send__(zns, :RatePlanData) do |rpd|
+          rpd.__send__(zns, :RatePlan) do |rp|
+            rp.__send__(ons, rate_plan_designation, product_rate_plan.id)
+          end
+        end
+      end
+    end
+
+    def generate_amend_options(builder)
+      amend_options.each do |k,v|
+        builder.__send__(zns, k.to_s.camelize.to_sym, v)
+      end
     end
   end
 end
--- spec/zuora/objects/amendment_spec.rb
+++ spec/zuora/objects/amendment_spec.rb
@@ -10,10 +10,226 @@ describe Zuora::Objects::Amendment do
     end
   end
 
-  it "validates date of term_start_date" do
-    subject.type = 'TermsAndConditions'
-    subject.term_start_date = 'invalid'
+  context "TermsAndConditions amendments" do
+    before do
+      subject.type = 'TermsAndConditions'
+      subject.should_not be_valid
+    end
+
+    it "validates date of term_start_date" do
+      subject.errors[:term_start_date].should include('is not a valid date')
+    end
+
+    it "validates that initial_term is a number if type is TermsAndConditions" do
+      subject.errors[:initial_term].should include("is not a number")
+    end
+
+    it "validates that renewal_term is a number if type is TermsAndConditions" do
+      subject.errors[:renewal_term].should include("is not a number")
+    end
+  end
+
+  it "requires name" do
+    subject.name = nil
     subject.should_not be_valid
-    subject.errors[:term_start_date].should include('is not a valid date')
+    subject.errors[:name].should include("can't be blank")
+  end
+
+  it "requires name to be less than 100 characters" do
+    subject.name = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+    subject.should_not be_valid
+    subject.errors[:name].should include("is too long (maximum is 100 characters)")
+  end
+
+  it "validates presence of product_rate_plan with certain types" do
+    ['NewProduct','RemoveProduct','UpdateProduct'].each do |amendment_type|
+      subject.type = amendment_type
+      subject.should_not be_valid
+      subject.errors[:product_rate_plan].should include("can't be blank")
+    end
+  end
+
+  describe "of product_rate_plan" do
+    it "validates that product_rate_plan is a pre-existing zuora object" do
+      subject.product_rate_plan = Zuora::Objects::ProductRatePlan.new
+      subject.valid?.should be_false
+      subject.errors[:product_rate_plan].should include('is invalid')
+    end
+
+    it "accepts a valid pre-existing zuora product_rate_plan" do
+      MockResponse.responds_with(:product_rate_plan_find_success) do
+        subject.product_rate_plan = Zuora::Objects::ProductRatePlan.find('stub')
+      end
+      subject.valid?
+      subject.errors[:product_rate_plan].should be_empty
+    end
+
+    describe "#rate_plan_designation" do
+      it "should return :AmendmentSubscriptionRatePlanId when type is RemoveProduct" do
+        subject.type = "RemoveProduct"
+        subject.rate_plan_designation.should eql :AmendmentSubscriptionRatePlanId
+      end
+
+      it "should return :AmendmentSubscriptionRatePlanId when type is UpdateProduct" do
+        subject.type = "UpdateProduct"
+        subject.rate_plan_designation.should eql :AmendmentSubscriptionRatePlanId
+      end
+
+      it "should return :ProductRatePlanId when type is AddProduct" do
+        subject.type = "AddProduct"
+        subject.rate_plan_designation.should eql :ProductRatePlanId
+      end
+    end
+  end
+
+  context "amendment requests" do
+    before do
+      MockResponse.responds_with(:subscription_find_success) do
+        subscription = Zuora::Objects::Subscription.find('stub')
+        subject.subscription = subscription
+        @end_date = subscription.subscription_end_date
+      end
+      subject.status = "Completed"
+    end
+
+    context "TermsAndConditions Amendments" do
+      it "generates proper xml request" do
+        subject.type = "TermsAndConditions"
+        subject.name = "Change Terms"
+
+        subject.contract_effective_date = @end_date + 1.day
+        subject.term_start_date = @end_date + 1.day
+        subject.initial_term = 1
+        subject.renewal_term = 1
+        subject.amend_options = {:generate_invoice => false, :process_payments => false}
+
+        MockResponse.responds_with(:amendment_success) do
+          subject.should be_valid
+          subject.create.should == true
+        end
+
+        xml = Zuora::Api.instance.last_request
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:Type").
+          with_value('TermsAndConditions')
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:Status").
+          with_value('Completed')
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:ContractEffectiveDate").
+          with_value('2012-08-21T00:00:00+00:00')
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:TermStartDate").
+          with_value('2012-08-21T00:00:00+00:00')
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:InitialTerm").
+          with_value('1')
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:RenewalTerm").
+          with_value('1')
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:Name").
+          with_value('Change Terms')
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:SubscriptionId").
+          with_value('4028e48834aa10a30134c9fcdf9f6764')
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:AmendOptions/#{zns}:GenerateInvoice").
+            with_value('false')
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:AmendOptions/#{zns}:ProcessPayments").
+          with_value('false')
+      end
+    end
+
+    context "Cancellation Amendments" do
+      it "generates proper xml request" do
+        subject.type = "Cancellation"
+        subject.name = "Cancel Subscription"
+        subject.contract_effective_date = @end_date + 1.day
+        subject.effective_date = @end_date + 1.day
+
+        MockResponse.responds_with(:amendment_success) do
+          subject.should be_valid
+          subject.create.should == true
+        end
+
+        xml = Zuora::Api.instance.last_request
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:Type").
+          with_value('Cancellation')
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:Status").
+          with_value('Completed')
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:ContractEffectiveDate").
+          with_value('2012-08-21T00:00:00+00:00')
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:EffectiveDate").
+          with_value('2012-08-21T00:00:00+00:00')
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:Name").
+          with_value('Cancel Subscription')
+        xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:SubscriptionId").
+          with_value('4028e48834aa10a30134c9fcdf9f6764')
+      end
+    end
+
+    context "Product Amendments" do
+      before do
+        MockResponse.responds_with(:product_rate_plan_find_success) do
+          subject.product_rate_plan = Zuora::Objects::ProductRatePlan.find('stub')
+        end
+
+        subject.contract_effective_date = Time.parse("2012-07-20 10:10:27-04:00")
+      end
+
+      context "AddProduct request" do
+        it "generates proper xml request" do
+          subject.type = 'NewProduct'
+          subject.name = 'Add Product'
+          subject.amend_options = {:generate_invoice => true, :process_payments => false}
+
+          MockResponse.responds_with(:amendment_success) do
+            subject.should be_valid
+            subject.create.should == true
+          end
+
+          xml = Zuora::Api.instance.last_request
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:Type").
+            with_value('NewProduct')
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:Status").
+            with_value('Completed')
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:ContractEffectiveDate").
+            with_value('2012-07-20T10:10:27-04:00')
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:Name").
+            with_value('Add Product')
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:SubscriptionId").
+            with_value('4028e48834aa10a30134c9fcdf9f6764')
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{zns}:RatePlanData/#{zns}:RatePlan/#{ons}:ProductRatePlanId").
+            with_value('4028e4883491c50901349d0e1e571341')
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:AmendOptions/#{zns}:GenerateInvoice").
+            with_value('true')
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:AmendOptions/#{zns}:ProcessPayments").
+            with_value('false')
+        end
+      end
+
+      context "RemoveProduct request" do
+        it "generates proper xml request" do
+          subject.type = 'RemoveProduct'
+          subject.name = 'Remove Product'
+          subject.amend_options = {:generate_invoice => false, :process_payments => false}
+
+          MockResponse.responds_with(:amendment_success) do
+            subject.should be_valid
+            subject.create.should == true
+          end
+
+          xml = Zuora::Api.instance.last_request
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:Type").
+            with_value('RemoveProduct')
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:Status").
+            with_value('Completed')
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:ContractEffectiveDate").
+            with_value('2012-07-20T10:10:27-04:00')
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:Name").
+            with_value('Remove Product')
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{ons}:SubscriptionId").
+            with_value('4028e48834aa10a30134c9fcdf9f6764')
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:Amendments/#{zns}:RatePlanData/#{zns}:RatePlan/#{ons}:AmendmentSubscriptionRatePlanId").
+            with_value('4028e4883491c50901349d0e1e571341')
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:AmendOptions/#{zns}:GenerateInvoice").
+            with_value('false')
+          xml.should have_xml("//env:Body/#{zns}:amend/#{zns}:requests/#{zns}:AmendOptions/#{zns}:ProcessPayments").
+            with_value('false')
+        end
+      end
+    end
   end
 end
