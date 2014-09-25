27592c4efc7fa7c42add80508b2025024f6e2d9e
James W Moline <jmoline@gmail.com>
2014-09-25 16:19:58 -0400

    oops, typo

--- lib/zuora/objects/subscribe_request.rb
+++ lib/zuora/objects/subscribe_request.rb
@@ -115,7 +115,7 @@ module Zuora::Objects
 
     def generate_subscription(builder)
       subscription.to_hash.each do |k,v|
-        builder.__send__(ons, k.to_s.zuora_camelize.to_sym, (v.is_a?(Date)||v.is_a?(Time) ? v.to_datetime.strftime("%FT%T%:z") : v) unless v.nil?
+        builder.__send__(ons, k.to_s.zuora_camelize.to_sym, (v.is_a?(Date)||v.is_a?(Time) ? v.to_datetime.strftime("%FT%T%:z") : v)) unless v.nil?
       end
     end
 
